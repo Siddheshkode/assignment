b41f56ad2b7ea04cd8fa4ce9eeb14cbaa6d58d1c
Bhavananande <44334631+Bhavananande@users.noreply.github.com>
2018-10-24 14:43:20 +0530

    Add files via upload

---
+++ bhavana.py
@@ -0,0 +1,13 @@
+list_of_digits=[0,1,2,3,4,]
+input_digit=int(input ("enter a digit"))
+for i in list_of_digits:
+if input_digits==i:
+    print("digit is in the list ")
+    break
+else:
+    print(" digit not found in list")
+        
+
+        	
+
+
